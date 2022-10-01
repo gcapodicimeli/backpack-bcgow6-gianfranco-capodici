5ff5629bf0d1cce39782f33365b2c136b9883fbd
--global <--global>
2022-10-01 13:15:57 -0300

    Complete C3 TT

--- C3-GO Bases 3 TT/ejercicio1.go
+++ C3-GO Bases 3 TT/ejercicio1.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import "fmt"
 
+type User struct {
+	name     string
+	lastName string
+	age      int
+	email    string
+	password string
+}
+
+func setName(u *User, newName, newLastName string) {
+	u.name = newName
+	u.lastName = newLastName
+}
+
+func setAge(u *User, newAge int) {
+	u.age = newAge
+}
+
+func setEmail(u *User, newEmail string) {
+	u.email = newEmail
+}
+
+func setPassword(u *User, newPassword string) {
+	u.password = newPassword
+}
+
+func main() {
+	user := User{
+		name:     "John",
+		lastName: "Doe",
+		age:      18,
+		email:    "john.doe@example.com",
+		password: "123qwe",
+	}
+
+	fmt.Printf("Nombre: %s\nApellido: %s\nAge: %d\nCorreo: %s\nContraseña: %s\n",
+		user.name, user.lastName, user.age, user.email, user.password,
+	)
+
+	setName(&user, "Juan", "Perez")
+	setAge(&user, 28)
+	setEmail(&user, "juan.peres@example.com")
+	setPassword(&user, "nuevapass")
+
+	fmt.Printf("Nombre: %s\nApellido: %s\nAge: %d\nCorreo: %s\nContraseña: %s\n",
+		user.name, user.lastName, user.age, user.email, user.password,
+	)
 }
