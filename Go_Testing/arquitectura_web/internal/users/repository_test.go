c1e69689b9ef63f4c187d937a2e04d269b547d18
--global <--global>
2022-10-20 16:46:24 -0300

    Probando..

--- Go_Testing/arquitectura_web/internal/users/repository_test.go
+++ Go_Testing/arquitectura_web/internal/users/repository_test.go
@@ -18,15 +18,14 @@ func (fs *StubStore) Read(data interface{}) error {
 
 func TestGetAll(t *testing.T) {
 	// Arrange
-	us := []User{
-		Id: 1,
-		Name: "Juan",
+	us := []User{Id: 1,
+		Name:     "Juan",
 		LastName: "Perez",
-		Email: "juan@gmail.com",
-		Age: 39,
-		Height: 1.78,
-		Active: true,
-		Data: "02-02-2022"
+		Email:    "juan@gmail.com",
+		Age:      39,
+		Height:   1.78,
+		Active:   true,
+		Data:     "02-02-2022",
 	}
 	myStubStore := &StubStore{Data: us}
 	repo := NewRepository(myStubStore)
