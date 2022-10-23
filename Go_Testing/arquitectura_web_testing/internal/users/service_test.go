71cda6471f7729282942173f8db69b60be4b66a4
--global <--global>
2022-10-23 16:00:52 -0300

    Test store fail

--- Go_Testing/arquitectura_web_testing/internal/users/service_test.go
+++ Go_Testing/arquitectura_web_testing/internal/users/service_test.go
@@ -65,3 +65,103 @@ func TestServiceIntegrationGetAllFail(t *testing.T) {
 	assert.EqualError(t, err, expectedError.Error())
 	assert.Nil(t, result)
 }
+
+// func TestServiceIntegrationStore(t *testing.T) {
+// 	// Arrange
+// 	initiaDatabase := []domain.User{
+// 		{Id: 1,
+// 			Name:     "Juan",
+// 			LastName: "Perez",
+// 			Email:    "juan@gmail.com",
+// 			Age:      39,
+// 			Height:   1.78,
+// 			Active:   true,
+// 			Date:     "02-02-2022",
+// 		},
+// 		{Id: 2,
+// 			Name:     "Juana",
+// 			LastName: "Lopez",
+// 			Email:    "juana@gmail.com",
+// 			Age:      79,
+// 			Height:   1.58,
+// 			Active:   true,
+// 			Date:     "02-06-2022",
+// 		},
+// 	}
+
+// 	expectedDatabase := []domain.User{
+// 		{Id: 1,
+// 			Name:     "Juan",
+// 			LastName: "Perez",
+// 			Email:    "juan@gmail.com",
+// 			Age:      39,
+// 			Height:   1.78,
+// 			Active:   true,
+// 			Date:     "02-02-2022",
+// 		},
+// 		{Id: 2,
+// 			Name:     "Juana",
+// 			LastName: "Lopez",
+// 			Email:    "juana@gmail.com",
+// 			Age:      79,
+// 			Height:   1.58,
+// 			Active:   true,
+// 			Date:     "02-06-2022",
+// 		},
+// 		{
+// 			Id:       3,
+// 			Name:     "Maria",
+// 			LastName: "Pereyra",
+// 			Email:    "maria@gmail.com",
+// 			Age:      45,
+// 			Height:   1.65,
+// 			Active:   true,
+// 			Date:     "12-09-2022",
+// 		},
+// 	}
+
+// 	mockStorage := MockStorage{
+// 		dataMock: initiaDatabase,
+// 	}
+
+// 	repository := NewRepository(&mockStorage)
+// 	service := NewService(repository)
+
+// 	// Act
+// 	result, err := service.Store("Maria", "Pereyra", "maria@gmail.com", 45, 1.65, true, "12-09-2022")
+// 	userCreated := domain.User{
+// 		Id:       3,
+// 		Name:     "Maria",
+// 		LastName: "Pereyra",
+// 		Email:    "maria@gmail.com",
+// 		Age:      45,
+// 		Height:   1.65,
+// 		Active:   true,
+// 		Date:     "12-09-2022",
+// 	}
+
+// 	// Assert
+// 	assert.Nil(t, err)
+// 	assert.Equal(t, userCreated, result)
+// 	assert.Equal(t, expectedDatabase, mockStorage.dataMock)
+// }
+
+func TestServiceIntegrationStoreFail(t *testing.T) {
+	// Arrange
+	expectedError := errors.New("Error on write a user")
+	mockStorage := MockStorage{
+		dataMock:   nil,
+		errOnRead:  nil,
+		errOnWrite: errors.New("Error on write a user"),
+	}
+
+	repository := NewRepository(&mockStorage)
+	service := NewService(repository)
+
+	// Act
+	result, err := service.Store("Maria", "Perez", "maria@gmail.com", 45, 1.65, true, "12-03-2022")
+
+	// Assert
+	assert.EqualError(t, err, expectedError.Error())
+	assert.Empty(t, result)
+}
